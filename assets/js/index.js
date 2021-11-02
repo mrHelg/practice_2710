2211a1e9722862399c63504bc4206903d3f29a34
Som&soft <som8soft@gmail.com>
2021-11-02 14:59:37 +0200

    Final version

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,33 +1,18 @@
 'use strict';
 
-const imagesDB = [
-  'https://previews.123rf.com/images/boligolovag/boligolovag2002/boligolovag200200560/140502050-aerial-wide-angle-panorama-of-zell-am-see-and-kaprun-tourist-resort-with-sunny-alp-mountain-summits-.jpg',
-  'https://images.vivino.com/thumbs/87V1aok6Scu9tnTHm7_OuA_pb_x600.png',
-  'https://img.cliparto.com/pic/xl/170570/3005382-square-seamless-blue-texture.jpg',
-];
+const image1 =
+  'https://previews.123rf.com/images/boligolovag/boligolovag2002/boligolovag200200560/140502050-aerial-wide-angle-panorama-of-zell-am-see-and-kaprun-tourist-resort-with-sunny-alp-mountain-summits-.jpg';
+const image2 =
+  'https://image.freepik.com/free-photo/beautiful-tropical-beach-sea-ocean-with-white-cloud-blue-sky-copyspace_74190-8663.jpg';
 
-const slider = new Slider(imagesDB);
-const img = document.querySelector('.slide>img');
-const [prevButton, nextButton] = document.querySelectorAll(
-  '.slider-container>button'
-);
+const img = document.querySelector('img');
+const btn = document.querySelector('button');
 
+let flag = true;
 function updateView() {
-  img.setAttribute('src', slider.currentSlide);
+  flag ? img.setAttribute('src', image1) : img.setAttribute('src', image2);
+  flag = !flag;
 }
+
 updateView();
-
-
-const btnSliderHandler = (direction = 'next') = 
-
-nextButton.addEventListener('click', () => {
-  slider.currentIndex = slider.nextIndex;
-  updateView();
-});
-
-prevButton.addEventListener('click', () => {
-  slider.currentIndex = slider.prevIndex;
-  updateView();
-});
-
-
+btn.addEventListener('click', updateView);
